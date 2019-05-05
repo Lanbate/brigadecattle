e4e37a01de01db727197aa2ce45b55b056d3fdc3
CJE黄家进 <huangjj12@centaline.com.cn>
2019-05-05 17:06:10 +0800

    add header

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from "./router";
 import store from "./store";
 import "./assets/styles/reset.css";
 import "./assets/styles/border.css";
+import "./assets/styles/iconfont.css";
 import fastClick from "fastclick";
 
 Vue.config.productionTip = false;
---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <div class="iconfont back-icon">&#xe624;</div>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe632;</span>
+      输入城市/景点/游玩主题
+    </div>
+    <div class="iconfont arrow-icon">城市&#xe62d;</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeHeader"
+};
+</script>
+
+<style lang="stylus" scoped>
+@import '~@/assets/styles/varibles.styl';
+.header
+    display :flex
+    line-height 0.86rem
+    background-color :$bgColor
+    color :#fff
+.header-left
+    width :.64rem
+    float left
+.header-input
+    flex:1
+    height .64rem
+    line-height .64rem
+    margin-top .12rem
+    margin-left :.2rem
+    padding-left :.2rem
+    background:#fff
+    border-radius :.1rem //5像素
+    color :#ccc
+.header-right
+    width :1.24rem
+    float :right
+    text-align: center
+.back-icon
+    text-align:center
+    font-size :.4rem
+.arrow-icon
+    padding .0.2rem
+    font-size :.27rem
+</style>
