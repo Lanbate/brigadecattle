43033197668a0fc7812d6acd4af83454b078475d
CJE黄家进 <huangjj12@centaline.com.cn>
2019-05-05 15:57:03 +0800

    project init

--- src/main.js
+++ src/main.js
@@ -2,10 +2,15 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import "./assets/styles/reset.css";
+import "./assets/styles/border.css";
+import fastClick from "fastclick";
 
 Vue.config.productionTip = false;
+fastClick.attach(document.body);
 
 new Vue({
+  el: "#app",
   router,
   store,
   render: h => h(App)
---
+++ src/pages/home/home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div class="home">home</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home"
+};
+</script>
+
+<style>
+.home {
+  font-size: 50px;
+}
+</style>
